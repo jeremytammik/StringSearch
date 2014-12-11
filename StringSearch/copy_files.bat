4df3d98358ecab38bb0d97168fcc3c1165ca15b3
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-12-11 00:00:01 +0000

    skip copying output files to parent folder

--- StringSearch/copy_files.bat
+++ StringSearch/copy_files.bat
@@ -9,12 +9,12 @@ echo copying StringSearch assembly and add-in manifest to Revit 2015 Addins fold
 copy %a%bin\Debug\ADNPlugin-StringSearch.dll "%AppData%\Autodesk\REVIT\Addins\2015"
 copy %a%ADNPlugin-StringSearch.addin "%AppData%\Autodesk\REVIT\Addins\2015"
 
-echo copying StringSearch assembly and add-in manifest to top level directory...
-copy %a%bin\Debug\ADNPlugin-StringSearch.dll %a%..
-copy %a%ADNPlugin-StringSearch.addin %a%..
+rem echo copying StringSearch assembly and add-in manifest to top level directory...
+rem copy %a%bin\Debug\ADNPlugin-StringSearch.dll %a%..
+rem copy %a%ADNPlugin-StringSearch.addin %a%..
 
-echo copying %a%ReadMe.txt to parent directory...
-copy %a%ReadMe.txt %a%..
+rem echo copying %a%ReadMe.txt to parent directory...
+rem copy %a%ReadMe.txt %a%..
 
 rem cd %a%..\..
 rem zip ..\zip\string_search ADNPlugin-StringSearch.addin ADNPlugin-StringSearch.dll ReadMe.txt Source\StringSearch.sln Source\StringSearch\
